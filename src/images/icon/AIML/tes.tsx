afb64614d30e3611ee0d234ea6111bf99c939fa6
GoSmart Tools <gosmarttools@gmail.com>
2026-02-23 08:08:20 +0700

    src/images/icon/AIML/tes.tsx

---
+++ src/images/icon/AIML/tes.tsx
@@ -0,0 +1,17 @@
+
+src/images/icon/Automation/tes.tsx
+src/images/icon/BaaS/tes.tsx
+src/images/icon/BackendDevelopment/tes.tsx
+src/images/icon/Database/tes.tsx
+src/images/icon/DataVisualization/tes.tsx
+src/images/icon/Devops/tes.tsx
+src/images/icon/Framework/tes.tsx
+src/images/icon/FrontendDevelopment/tes.tsx
+src/images/icon/GameEngines/tes.tsx
+src/images/icon/MobileAppDevelopment/tes.tsx
+src/images/icon/Other/tes.tsx
+src/images/icon/ProgrammingLanguages/tes.tsx
+src/images/icon/Social/tes.tsx
+src/images/icon/Software/tes.tsx
+src/images/icon/StaticSiteGenerators/tes.tsx
+src/images/icon/Testing/tes.tsx
