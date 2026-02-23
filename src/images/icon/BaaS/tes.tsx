c393a40e70bc5b8681c95b334ee598f8cf08a619
GoSmart Tools <gosmarttools@gmail.com>
2026-02-23 08:13:57 +0700

    src/images/icon/BaaS/tes.tsx

---
+++ src/images/icon/BaaS/tes.tsx
@@ -0,0 +1,15 @@
+
+src/images/icon/BackendDevelopment/tes.tsx
+src/images/icon/Database/tes.tsx
+src/images/icon/DataVisualization/tes.tsx
+src/images/icon/Devops/tes.tsx
+src/images/icon/Framework/tes.tsx
+src/images/icon/FrontendDevelopment/tes.tsx
+src/images/icon/GameEngines/tes.tsx
+src/images/icon/MobileAppDevelopment/tes.tsx
+src/images/icon/Other/tes.tsx
+src/images/icon/ProgrammingLanguages/tes.tsx
+src/images/icon/Social/tes.tsx
+src/images/icon/Software/tes.tsx
+src/images/icon/StaticSiteGenerators/tes.tsx
+src/images/icon/Testing/tes.tsx
